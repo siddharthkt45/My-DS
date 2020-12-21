363e9fd63eee56052f5177c70cafb03dbc5bc052
Siddharth Tiwari <siddharthkt45@gmail.com>
2020-12-21 12:18:36 +0530

    program for Parenthesis matching added

---
+++ Stack/ParenthesisMatching.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    char data;
+    struct Node *next;
+} *top = NULL;
+
+void push(char x)
+{
+    struct Node *t = (struct Node *)malloc(sizeof(struct Node));
+    if(t == NULL)
+        printf("Stack overflow");
+    else
+    {
+        t->data = x;
+        t->next = top;
+        top = t;
+    }
+}
+
+char pop()
+{
+    struct Node *t;
+    int x = -1;
+    if(top == NULL)
+        printf("Stack empty");
+    else
+    {
+        t = top;
+        top = top->next;
+        x = t->data;
+        free(t);
+    }
+    return x;
+}
+
+void display()
+{
+    struct Node *p = top;
+    while(p)
+    {
+        printf("%d ", p->data);
+        p = p->next;
+    }
+    printf("\n");
+}
+
+int isBalanced(char *exp)
+{
+    for(int i = 0; exp[i] != '\0'; i++)
+    {
+        if(exp[i] == '(')
+            push(exp[i]);
+        else if(exp[i] == ')')
+        {
+            if(top == NULL)
+                return 0;
+            pop();
+        }
+    }
+    if(top == NULL)
+        return 1;
+    else
+        return 0;
+}
+
+int main()
+{
+    char *exp = "((a+b)*(c-d)))";
+    printf("%d\n", isBalanced(exp));
+    return 0;
+}
--- Stack/StackLL.cpp
+++ Stack/StackLL.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
 struct Node
 {
-    int data;
+    char data;  // int data;
     struct Node *next;
 } *top = NULL;
 
@@ -47,15 +48,116 @@ void display()
     cout << endl;
 }
 
+int pre(char x)
+{
+    if(x == '+' || x == '-')
+        return 1;
+    else if(x == '*' || x == '/')
+        return 2;
+    return 0;
+}
+
+int isOperand(char x)
+{
+    if (x == '+' || x == '-' || x == '*' || x == '/')
+        return 0;
+    return 1;
+}
+
+int isBalanced(char *exp)
+{
+    for (int i = 0; exp[i] != '\0'; i++)
+    {
+        if (exp[i] == '(')
+            push(exp[i]);
+        else if (exp[i] == ')')
+        {
+            if (top == NULL)
+                return 0;
+            pop();
+        }
+    }
+    if (top == NULL)
+        return 1;
+    else
+        return 0;
+}
+
+char * InToPost(char *infix)
+{
+    int i = 0, j = 0;
+    char *postfix;
+    postfix = new char[strlen(infix) + 2];
+
+    while(infix[i] != '\0')
+    {
+        if(isOperand(infix[i]))
+            postfix[j++] = infix[i++];
+        else
+        {
+            if(pre(infix[i]) > pre(top->data))
+                push(infix[i++]);
+            else
+                postfix[j++] = pop();
+        }
+    }
+    while(top != NULL)
+        postfix[j++] = pop();
+    
+    postfix[j] = '\0';
+    return postfix;
+}
+
+int Eval(char *postfix)
+{
+    int x1, x2, r = 0;
+
+    for(int i = 0; postfix[i] != '\0'; i++)
+    {
+        if(isOperand(postfix[i]))
+            push(postfix[i] - '0');
+        else
+        {
+            x2 = pop();
+            x1 = pop();
+            switch(postfix[i])
+            {
+            case '+':
+                r = x1 + x2;
+                break;
+            case '-':
+                r = x1 - x2;
+                break;
+            case '*':
+                r = x1 * x2;
+                break;
+            case '/':
+                r = x1/x2;
+                break;
+            default:
+                break;
+            }
+            push(r);
+        }
+    }
+    return top->data;
+}
+
 int main()
 {
-    push(10);
-    push(20);
-    push(30);
+    char *infix = "a+b*c-d/e";
+    push('#');
 
-    display();
+    char *postfix = InToPost(infix);
+    printf("%s ", postfix);
+    cout << endl << "# marks the end of the postfix expression";
+    // push(10);
+    // push(20);
+    // push(30);
 
-    cout << pop() << " ";
+    // display();
+
+    // cout << pop() << " ";
 
     return 0;
 }
